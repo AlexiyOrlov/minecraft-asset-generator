82078bfc268a8d838d33edee05f76a2fd40ff039
Alexiy <alexiy.ov@gmail.com>
2020-11-22 11:44:47 +0200

    Convert to java code

---
+++ src/main/groovy/alexiy/minecraft/assetgenerator/AssetConstants.java
@@ -0,0 +1,14 @@
+package alexiy.minecraft.assetgenerator;
+
+/**
+ * Created on 8/7/17.
+ */
+public enum AssetConstants {
+    ASSETS("assets"), MODELS_LITERAL("models"), BLOCKMODEL("block"), ITEMMODEL("item"), BLOCKSTATES("blockstates"), TEXTURES("textures"), BLOCKTEXTURE("blocks"), ITEMTEXTURE("items"), BLOCK_LITERAL("block"), ITEM_LITERAL("item"), DATA("data"), LOOT_TABLES("loot_tables"), BLOCKS_LITERAL("blocks");
+
+    AssetConstants(String v) {
+        value = v;
+    }
+
+    public String value;
+}
